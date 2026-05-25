03f0f77296d95ee79cc7c3bb28c9d6258345066a
lowskydev <lowskydev@gmail.com>
2026-05-25 21:26:30 +0200

    fix sqlite path in docker using separate data volume

--- app/database.py
+++ app/database.py
@@ -1,8 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./trivia.db"
+DB_DIR = os.environ.get("DB_DIR", ".")
+os.makedirs(DB_DIR, exist_ok=True)
+
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_DIR}/trivia.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
